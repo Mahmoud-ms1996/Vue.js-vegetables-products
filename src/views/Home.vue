7109f15b263880982b03ab3d84802cdf832ba050
Mahmoud-ms1996 <mahmoud.masri1996@gmail.com>
2022-06-28 00:29:03 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div class="splash-container">
+        <div class="splash">
+          <h1>Splendid Food</h1>
+        </div>
+      </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <ProductCard
+          class="card"
+          v-for="(product, index) in inventory.slice(0,3)"
+          :key="product.id"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+        />
+
+      </div>
+
+    </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductCard from '@/components/product-card.vue'
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="wrapper">
+        <h1>Products</h1>
+
+        <div class="card-container">
+          <ProductCard
+            class="card"
+            v-for="(product, index) in inventory"
+            :key="product.id"
+            :index="index"
+            :product="product"
+            :addToCart="addToCart"
+          />
+        </div>
+      </main>
+</template>
+
+<script>
+import ProductCard from '@/components/product-card.vue'
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Products',
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
+
+<style>
+</style>
